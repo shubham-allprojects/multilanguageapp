23d7acb8482cce1b1df31dbaf394b08731033e5c
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2022-11-11 11:54:50 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
-import { useTranslation, Trans } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import { useEffect } from "react";
+import Navbar from "./components/1.Navbar/Navbar";
+import About from "./components/2.About/About";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const { i18n } = useTranslation();
@@ -18,76 +21,12 @@ function App() {
   });
 
   return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            Amazon
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item pe-2">
-                <a className="nav-link" aria-current="page" href="/">
-                  <Trans>About Us</Trans>
-                </a>
-              </li>
-              <li className="nav-item pe-2">
-                <a className="nav-link" href="/">
-                  <Trans>Features</Trans>
-                </a>
-              </li>
-              <li className="nav-item pe-2">
-                <a className="nav-link" href="/">
-                  <Trans>Pricing</Trans>
-                </a>
-              </li>
-
-              <li className="nav-item pe-2">
-                <select
-                  name="languages"
-                  className="form-select"
-                  onChange={changeLanguage}
-                >
-                  <option id="en" value="en">
-                    English
-                  </option>
-                  <option id="mar" value="mar">
-                    मराठी
-                  </option>
-                  <option id="hin" value="hin">
-                    हिंदी
-                  </option>
-                </select>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <div className="container-fluid py-5">
-        <div className="row justify-content-center py-5">
-          <div className="col-lg-5 col-md-6">
-            <Trans>
-              Technology is our new present and future, making human lives
-              easier and more convenient. With new technological innovations,
-              human beings have become more dependent on science and technology.
-              We rely on technology for all our activities and have become an
-              integral part of our lives.
-            </Trans>
-          </div>
-        </div>
-      </div>
-    </>
+    <BrowserRouter>
+      <Navbar changeLanguage={changeLanguage} />
+      <Routes>
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/2.About/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Trans } from "react-i18next";
+
+const About = () => {
+  return (
+    <div className="container-fluid py-5">
+      <div className="row justify-content-center py-5">
+        <div className="col-lg-5 col-md-6">
+          <Trans>
+            Technology is our new present and future, making human lives easier
+            and more convenient. With new technological innovations, human
+            beings have become more dependent on science and technology. We rely
+            on technology for all our activities and have become an integral
+            part of our lives.
+          </Trans>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;

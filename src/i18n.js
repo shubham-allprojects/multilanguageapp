3b547f1bb3f9d9578f6f8440814c2836f3d8df74
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2022-11-15 11:02:54 +0530

    ok

--- src/i18n.js
+++ src/i18n.js
@@ -1,17 +1,16 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import hindiText from "./components/4.TranslationData/hindi.json";
-import marathiText from "./components/4.TranslationData/marathi.json";
+import Language from "./components/4.TranslationData/TranslationData.json";
 
 const resources = {
   en: {
     translation: {},
   },
   hin: {
-    translation: hindiText,
+    translation: Language.hin,
   },
   mar: {
-    translation: marathiText,
+    translation: Language.mar,
   },
 };
 
